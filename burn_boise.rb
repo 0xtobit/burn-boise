767f1e94fe2702eca6ade2d81c2def7e5e4f5afb
Tobit Raff <0xtobit@gmail.com>
2019-11-06 12:55:04 -0700

    Move to erb template. Use zipcode as variable

--- burn_boise.rb
+++ burn_boise.rb
@@ -1,9 +1,12 @@
 require 'sinatra'
 require 'httparty'
 require 'nokogiri'
+require 'erb'
 
 get '/' do
-  doc = HTTParty.get('https://airnow.gov/index.cfm?action=airnow.local_city&zipcode=83704&submit=Go')
+  zipcode = "83704"
+  doc = HTTParty.get("https://airnow.gov/index.cfm?action=airnow.local_city&zipcode=#{zipcode}&submit=Go")
   parsed_page ||= Nokogiri::HTML(doc)
-  parsed_page.css('.TblInvisible')[0].text[/\d+/] # .to_i
+  aqi = parsed_page.css('.TblInvisible')[0].text[/\d+/].to_i
+  erb File.open('erb/index.html.erb').read, locals: { aqi: aqi }
 end
