3171f216835d1578e9f8738d36e012b11acea71c
Tobit Raff <0xtobit@gmail.com>
2019-11-06 14:06:02 -0700

    Add styling and messages

--- burn_boise.rb
+++ burn_boise.rb
@@ -5,8 +5,27 @@ require 'erb'
 
 get '/' do
   zipcode = "83704"
-  doc = HTTParty.get("https://airnow.gov/index.cfm?action=airnow.local_city&zipcode=#{zipcode}&submit=Go")
+  url = "https://airnow.gov/index.cfm?action=airnow.local_city&zipcode=#{zipcode}&submit=Go"
+  doc = HTTParty.get(url)
   parsed_page ||= Nokogiri::HTML(doc)
   aqi = parsed_page.css('.TblInvisible')[0].text[/\d+/].to_i
-  erb File.open('erb/index.html.erb').read, locals: { aqi: aqi }
+
+  # http://www2.deq.idaho.gov/air/AQIPublic/Ordinance/Index/3
+  case aqi
+  when 0..59
+    background_color = 'green'
+    message = 'Burn baby, burn!'
+  when 60..73
+    background_color = 'orange'
+    message = "Maybe don't..."
+  when 74..1000
+    background_color = 'dark-red'
+    message = "Nope."
+  else
+    background_color = 'white'
+    message = "idk..."
+  end
+
+  erb File.open('erb/index.html.erb').read, locals: { aqi: aqi, background_color: background_color, message: message,
+                                                      url: url}
 end
